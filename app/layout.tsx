a190d0deb6830950a3c8d233df02f2a64354f5d4
aXenDeveloper <axendeveloper@gmail.com>
2022-11-27 20:47:59 +0100

    Add detected language from browser in SSR

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
-import './globals.css'
+import { headers } from 'next/headers';
+import './globals.css';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+async function getLanguage() {
+  const headersInstance = headers();
+  return headersInstance.get('accept-language');
+}
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const lang = await getLanguage();
+
+  console.log(lang);
+
   return (
-    <html lang="en">
+    <html lang={lang as string}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -14,5 +20,5 @@ export default function RootLayout({
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
